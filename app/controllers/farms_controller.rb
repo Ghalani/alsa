eadd31c85931bcc0259b2d39215b1deb6c26081c
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-11-01 20:04:50 +0000

    Add User to Org dashboard

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,6 +1,6 @@
 class FarmsController < ApplicationController
-  before_action :set_farm, only: [:show, :edit, :update, :destroy]
   before_action :set_organization
+  before_action :set_farm, only: [:show, :edit, :update, :destroy]
 
   # GET /farms
   # GET /farms.json
@@ -79,7 +79,12 @@ class FarmsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_farm
-      @farm = Farm.find(params[:id])
+      begin
+        @farm = Farm.find(params[:id])
+      rescue
+        flash[:notice] = "This farm is no longer in your system..."
+        redirect_to organization_farm_and_labour_path(@organization)
+      end
     end
 
     def set_organization
