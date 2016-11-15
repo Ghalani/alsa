9591834340f11e06de1c7fdc17bc8efe8e20d99c
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-11-15 16:16:40 +0000

    Finish Location and Hierarchy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -115,3 +115,9 @@ function prune(array, id) {
         }
     }
 }
+
+function cancelParentEvent(){
+  if (!e) var e = window.event;
+  e.cancelBubble = true;
+  if (e.stopPropagation) e.stopPropagation();
+}
