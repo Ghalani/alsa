fe26ee26123cc5a7df30b3c3f1847105895eaea3
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-11-20 21:34:32 +0000

    Prepping for master

--- app/serializers/ordered_stock_serializer.rb
+++ app/serializers/ordered_stock_serializer.rb
@@ -2,5 +2,5 @@ class OrderedStockSerializer < ActiveModel::Serializer
   has_one :stock_item
   has_one :receiver
 
-  attributes :id, :customer_order_id, :quantity, :cost
+  attributes :id, :stock_item_id, :customer_order_id, :quantity, :cost
 end
